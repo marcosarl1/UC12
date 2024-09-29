9facf775b7f25b372b6119dc4ad2f55b8addfbbc
Marcos Júnior <marcosajr11@gmail.com>
2024-09-28 22:51:36 -0300

    feat: Criado teste para ConvenioDAO

--- test/dao/ConvenioDAOTest.java
+++ test/dao/ConvenioDAOTest.java
@@ -1,16 +1,34 @@
 package dao;
 
+import modelo.Convenio;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class ConvenioDAOTest {
 
-    @Test
-    public void buscarcConvenios() {
+    private ConvenioDAO convenioDAO;
+
+    @Before
+    public void setUp() {
+        convenioDAO = new ConvenioDAO();
     }
 
     @Test
-    public void buscarConvenioFiltro() {
+    public void buscarcConvenios() throws SQLException {
+        List<Convenio> convenios = convenioDAO.buscarcConvenios();
+        assertNotNull(convenios);
+        assertFalse(convenios.isEmpty());
+    }
+
+    @Test
+    public void buscarConvenioFiltro() throws SQLException {
+        Convenio convenio = convenioDAO.buscarConvenioFiltro("Unimed");
+        assertNotNull(convenio);
+        assertEquals("Unimed", convenio.getNomeConvenio());
     }
 }
